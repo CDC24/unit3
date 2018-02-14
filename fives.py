19a9db3b6e9246e7db5e2f16b90f43bbcbc46730
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-14 08:37:16 -0500

    Updated from Brython Server: 2/14/2018 8:37:15 AM

--- fives.py
+++ fives.py
@@ -1,3 +1,11 @@
 #Caleb Callaway
 #2/14/18
 #fives.py - asks for number and prints multiples of 5 up to that number
+
+
+num = int(input("Enter a number: "))
+
+i = 0
+while i <= num:
+    print i
+    i+=5
