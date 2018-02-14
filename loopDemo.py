c873a997dc8e8dc8e4a00ea777228013abef9a07
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-14 08:11:09 -0500

    Updated from Brython Server: 2/14/2018 8:11:08 AM

--- loopDemo.py
+++ loopDemo.py
@@ -1,3 +1,9 @@
 #Caleb Callaway
 #2/14/18
-#loopDemo.py -using loops
+#loopDemo.py -using for and while loops
+
+
+# i love computer science five times without copy and paste or *5
+
+for i in range(1,5):
+    print("Yoot Yoot!")
