4b9f1bf14761c3b479aca002104445672a84cd3f
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-14 08:33:18 -0500

    Updated from Brython Server: 2/14/2018 8:33:17 AM

--- favorites.py
+++ favorites.py
@@ -1,3 +1,10 @@
 #Caleb Callaway
 #2/14/18
 #favorites.py -using loops to print inputted words a number of times
+
+
+word = input("What is your favorite word? ")
+num = input("What is your favorite number? ")
+
+for i in range (1,num+1):
+    print (word)
