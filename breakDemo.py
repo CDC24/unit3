5021f1b3625fccdf279ccefbe2ec8ceccfd66aa9
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-15 08:06:34 -0500

    Updated from Brython Server: 2/15/2018 8:06:33 AM

--- breakDemo.py
+++ breakDemo.py
@@ -2,6 +2,15 @@
 #2/15/18
 #breakDemo.py - using the break command for loops
 
+
+"""
 while 1==1:
     text = input("say anything: ")
+    print (text)
+""" 
+ 
+ while True:     #true does the same as 1==1
+    text = input("say anything: ")
+    if text == "anything":
+         break
     print (text)
