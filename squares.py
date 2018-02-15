606d41f2a447e1b5e09aaab9638e79944a5204b5
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-15 08:15:42 -0500

    Updated from Brython Server: 2/15/2018 8:15:41 AM

--- squares.py
+++ squares.py
@@ -1,3 +1,9 @@
 #Caleb Callaway
 #2/15/18
-#squares.py - prints square numbers
+#squares.py - prints an entered number of square numbers
+
+
+num = int(input("enter a number"))
+
+for i in range (1,num+1):
+    print (i**2)
