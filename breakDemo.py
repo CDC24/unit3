a9021f7b394f5c5e5621da534409f01fae958f20
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-15 08:04:41 -0500

    Updated from Brython Server: 2/15/2018 8:04:40 AM

--- breakDemo.py
+++ breakDemo.py
@@ -1,3 +1,7 @@
 #Caleb Callaway
 #2/15/18
-#breakDemo.py - using the break command in loops
+#breakDemo.py - using the break command for loops
+
+while 1==1:
+    text = input("say anything: ")
+    print (text)
