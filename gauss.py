b11434baa13b47c38242612d7a2bbaea500e40bb
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-15 08:47:43 -0500

    Updated from Brython Server: 2/15/2018 8:47:42 AM

--- gauss.py
+++ gauss.py
@@ -6,6 +6,7 @@ start = int(input("Enter the beginning number of the range: "))
 end = int(input("Enter the end number of the range: "))
 space = int(input("Enter how much to go up by each time: "))
 
+total = 0
 for i in range (start, end+1):
     i+=space
     total = total+i
