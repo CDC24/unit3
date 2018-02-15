30d65bedeec1959031ddf790005c48f37cd13f09
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-15 08:42:18 -0500

    Updated from Brython Server: 2/15/2018 8:42:17 AM

--- gauss.py
+++ gauss.py
@@ -1,3 +1,11 @@
 #Caleb Callaway
 #2/15/18
 #gauss.py - prints out sum of numbers in a range
+
+start = int(input("Enter the beginning number of the range: "))
+end = int(input("Enter the end number of the range: "))
+space = int(input("Enter how much to go up by each time: "))
+
+for i in range (start, end+1):
+    i+=space
+    print i
