f874448f5d2bd1eabda30fc6fa7260971908026c
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-26 08:40:08 -0500

    Updated from Brython Server: 2/26/2018 8:40:08 AM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -11,7 +11,7 @@ i = 0
 
 while True:
     i+=1
-    guess= input ("Guess a number from 1 to 100.")
+    guess= int(input("Guess a number from 1 to 100."))
     if guess == num:
         print ("you got it in",i,"guesses!")
         break
