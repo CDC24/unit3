ba0a55fa5e381053bdd8ff80ca5a53eff0db1a96
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-26 08:24:13 -0500

    Updated from Brython Server: 2/26/2018 8:24:12 AM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,3 +1,10 @@
 #Caleb Callaway
 #2/26/18
 #hitchhiker.py - asks for questions until break command
+
+
+while True:
+    q = input ("Ask a question or type 'stop': ")
+    if q == ("stop"):
+        break
+    
