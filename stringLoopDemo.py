854a0b828b44213e2491930abc3ec2b9b0ab6fee
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-26 08:04:41 -0500

    Updated from Brython Server: 2/26/2018 8:04:40 AM

--- stringLoopDemo.py
+++ stringLoopDemo.py
@@ -1,3 +1,9 @@
 #Caleb Callaway
 #2/26/18
 #stringLoopDemo - how to use strings in loop programs
+
+
+name = input ("Enter your name. ")
+
+for ch in name: #ch stands for character but can be anything
+    print (ch)
