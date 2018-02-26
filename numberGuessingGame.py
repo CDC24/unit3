4db8dcb8e711c248517657961af99c13590dfeb6
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-26 08:39:04 -0500

    Updated from Brython Server: 2/26/2018 8:39:04 AM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,3 +1,16 @@
 #Caleb Callaway
 #2/26/18
 #numberGuessingGame.py- uses loops to make a number guessing game, prints out tries
+
+
+from random import randint
+
+num = randint (1,100)
+
+i = 0
+
+while True:
+    i+=1
+    guess= input ("Guess a number from 1 to 100.")
+    if guess == num:
+        print ("you got it in",i,"guesses!")
