9333401b7f950c58f98554a3b72dead3d4fad777
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-28 08:27:20 -0500

    Updated from Brython Server: 2/28/2018 8:27:20 AM

--- warmUp8.py
+++ warmUp8.py
@@ -1,3 +1,9 @@
 #Caleb Callaway
 #2/28/18
 #warmUp8.py - finds sum of all numbers less than 100,000 that are divisible by 3, 10, and 17
+
+
+for i in range (0, 100001):
+    if i//3==0 and i//10==0 and 17//17==0:
+        print i
+    i +=1
