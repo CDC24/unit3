90d25f04c2bf4ed2dbea2c6ec9bf9c99b46f39c1
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-28 08:29:54 -0500

    Updated from Brython Server: 2/28/2018 8:29:53 AM

--- warmUp8.py
+++ warmUp8.py
@@ -2,8 +2,10 @@
 #2/28/18
 #warmUp8.py - finds sum of all numbers less than 100,000 that are divisible by 3, 10, and 17
 
-
+total = 0
 for i in range (0, 100001):
     if i%3==0 and i%10==0 and i%17==0:
         print (i)
+        total += i
     i +=1
+print (total)
