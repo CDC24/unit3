5bd8261658be2dde6fdaaea1138be48c44f9f4e3
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-01 08:31:46 -0500

    Updated from Brython Server: 3/1/2018 8:31:45 AM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,3 +1,12 @@
 #Caleb Callaway
-#2/28/18
-#perfectNumber.py- uses loops to list divisors
+#3/1/18
+#perfectNumber.py- determines whether or not something is a perfect number
+
+num = int(input("Enter a number: "))
+
+total=0
+for i in range (1,num):
+    if num%i==0:
+        total+=i
+print (total)
+
